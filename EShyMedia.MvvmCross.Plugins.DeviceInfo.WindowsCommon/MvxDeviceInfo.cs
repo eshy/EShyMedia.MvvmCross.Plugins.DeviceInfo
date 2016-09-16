256dcbaf466bd4738d14e13fb59e0c97ffffe320
Ishai Hachlili <ishai@ishaihachlili.com>
2016-09-16 13:09:52 -0700

    Added support for OS version in UWP apps and updated to MvvmCross 4.2.3

--- EShyMedia.MvvmCross.Plugins.DeviceInfo.WindowsCommon/MvxDeviceInfo.cs
+++ EShyMedia.MvvmCross.Plugins.DeviceInfo.WindowsCommon/MvxDeviceInfo.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Reflection;
 using Windows.Networking.Connectivity;
 using Windows.Security.ExchangeActiveSyncProvisioning;
 using Windows.Storage.Streams;
@@ -22,7 +24,7 @@ namespace EShyMedia.MvvmCross.Plugins.DeviceInfo.WindowsCommon
             {
                 DeviceType = os,
                 DeviceName = friendly,
-                SoftwareVersion = "8.1",//TODO: check if 10 introduced a way to get this info
+                SoftwareVersion = GetUWPVersion(),
                 Manufacturer = manufacturer,
                 HardwareVersion = product,
                 HardwareId = GetHardwareId(),
@@ -54,6 +56,44 @@ namespace EShyMedia.MvvmCross.Plugins.DeviceInfo.WindowsCommon
 
             return BitConverter.ToString(bytes);
         }
+
+        /// <summary>
+        /// Use reflection to get UWP version if running on Win10, return "8.1" if not
+        /// </summary>
+        /// <returns></returns>
+        private string GetUWPVersion()
+        {
+            var analyticsInfoType = Type.GetType(
+              "Windows.System.Profile.AnalyticsInfo, Windows, ContentType=WindowsRuntime");
+            var versionInfoType = Type.GetType(
+              "Windows.System.Profile.AnalyticsVersionInfo, Windows, ContentType=WindowsRuntime");
+            if (analyticsInfoType == null || versionInfoType == null)
+            {
+                Debug.WriteLine("Apparently you are not on Windows 10");
+                return "8.1";
+            }
+
+            var versionInfoProperty = analyticsInfoType.GetRuntimeProperty("VersionInfo");
+            var versionInfo = versionInfoProperty.GetValue(null);
+            var versionProperty = versionInfoType.GetRuntimeProperty("DeviceFamilyVersion");
+            var familyVersion = versionProperty.GetValue(versionInfo);
+
+            long versionBytes;
+            if (!long.TryParse(familyVersion.ToString(), out versionBytes))
+            {
+                Debug.WriteLine("Can't parse version number");
+                return "10.0";
+            }
+
+            var uapVersion = new Version((ushort)(versionBytes >> 48),
+              (ushort)(versionBytes >> 32),
+              (ushort)(versionBytes >> 16),
+              (ushort)(versionBytes));
+
+            return uapVersion.ToString();
+        }
+
+
 
     }
 }
